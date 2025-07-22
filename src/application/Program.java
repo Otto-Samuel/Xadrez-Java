c3a27f5fc4e286ece5bfd3dbf9cd20596cff42b2
Otto Samuel <162514493+Otto-Samuel@users.noreply.github.com>
2025-07-22 11:47:09 -0300

    ADD: description to project

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,13 @@
 package application;
 
 import boardgame.Board;
-//import boardgame.Position;
+
+/*
+ * autor: Otto Samuel
+ * data: 19/07/2025
+ * descrição: programa para testar o jogo de xadrez
+ * versão: 1.0
+ */
 
 public class Program {
     public static void main(String[] args) {
