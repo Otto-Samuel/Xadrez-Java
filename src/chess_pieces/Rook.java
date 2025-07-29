b2da78d94fd78a4a525271d75bf2524913682e09
Otto Samuel <162514493+Otto-Samuel@users.noreply.github.com>
2025-07-29 16:37:04 -0300

    Update: Possible moves of a piece

--- src/chess_pieces/Rook.java
+++ src/chess_pieces/Rook.java
@@ -5,12 +5,18 @@ import chess.ChessPiece;
 import chess.Color;
 
 public class Rook extends ChessPiece {
-    public Rook(Board board, Color color){
+    public Rook(Board board, Color color) {
         super(board, color);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "R";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
     }
 }
